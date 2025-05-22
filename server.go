1f8e659583396e9cde15a2357c2df5ce16793033
Felix Drolshagen <felix.drolshagen@gmail.com>
2025-05-22 09:36:47 +0200

    read routes from static directory

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -12,6 +13,7 @@ import (
 )
 
 var PORT int = 9999
+
 var StatusCodes map[int]string = map[int]string{
 	200: "ok",
 	404: "not found",
@@ -57,30 +59,49 @@ func handleConnection(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
-		log.Println(err)
+		if err == io.EOF {
+			return
+		}
+		log.Printf("Cannot read Request %s", err)
 		return
 	}
 
 	httpRequest, err := parseHttpRequest(buf)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Cannot parse HttpRequest %s", err)
+		return
 	}
 
 	var httpResponse HttpResponse
 
-	// TODO implement route handler, lookup files from directory
+	var requestedPath string
 	if httpRequest.url == "/" {
-		httpResponse.body = "<html><body><h1>Hello world</h1></body></html>"
+		requestedPath = "index.html"
+	} else {
+		requestedPath = httpRequest.url
+	}
+
+	httpResponse.body, err = readFile(requestedPath)
+	if err != nil {
+		httpResponse.statusCode = 404
+	} else {
 		httpResponse.contentType = "text/html"
 		httpResponse.statusCode = 200
-	} else {
-		httpResponse.statusCode = 404
 	}
 
 	log.Printf("Incoming request: %s %s -> %d", httpRequest.method, httpRequest.url, httpResponse.statusCode)
 
 	response := buildHttpReponse(httpResponse)
 	conn.Write([]byte(response))
+}
+
+func readFile(url string) (string, error) {
+	file, err := os.ReadFile("./static/" + url)
+	if err != nil {
+		return "", err
+	}
+
+	return string(file), nil
 }
 
 func parseHttpRequest(b []byte) (HttpRequest, error) {
@@ -117,10 +138,13 @@ func parseHttpRequest(b []byte) (HttpRequest, error) {
 func buildHttpReponse(httpResponse HttpResponse) string {
 	var b strings.Builder
 
-	fmt.Fprintf(&b, "HTTP/1.0 %d %s\r\n", httpResponse.statusCode, StatusCodes[httpResponse.statusCode])
+	fmt.Fprintf(&b, "HTTP/1.1 %d %s\r\n", httpResponse.statusCode, StatusCodes[httpResponse.statusCode])
 	fmt.Fprintf(&b, "Date: %s\r\n", getHttpDate())
 	fmt.Fprintf(&b, "Server: GO-HTTP (0.1)\r\n")
-	fmt.Fprintf(&b, "Content-type: %s\r\n", httpResponse.contentType)
+
+	if httpResponse.contentType != "" {
+		fmt.Fprintf(&b, "Content-type: %s\r\n", httpResponse.contentType)
+	}
 	fmt.Fprintf(&b, "\r\n")
 
 	fmt.Fprintf(&b, httpResponse.body)
