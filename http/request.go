19aa8a5bf2db676a6548d2a7a53f352c27cefc54
Felix Drolshagen <felix.drolshagen@gmail.com>
2025-06-05 22:28:11 +0200

    refactor parseRequest, split into mutliple functions

--- http/request.go
+++ http/request.go
@@ -21,46 +21,72 @@ type Request struct {
 
 // ParseRequest parses a []byte and tries to construct a valid Request.
 // Returns an error if the []byte cannot be parsed as a valid HTTP-Request
-func ParseRequest(b []byte) (Request, error) {
-	var req Request
+func ParseRequest(b []byte) (req Request, err error) {
+	header, body, err := splitHeaderAndBody(b)
+	if err != nil {
+		return req, err
+	}
+	req.Body = body
 
+	headerLines, err := splitHeader(header)
+	if err != nil {
+		return req, err
+	}
+
+	if err := parseRequestLine(headerLines[0], &req); err != nil {
+		return req, err
+	}
+
+	if err := parseHeaders(headerLines[1:], &req); err != nil {
+		return req, err
+	}
+
+	return req, nil
+}
+
+func splitHeaderAndBody(b []byte) ([]byte, []byte, error) {
 	separator := []byte("\r\n\r\n")
 	idx := bytes.Index(b, separator)
 	if idx < 0 {
-		return req, fmt.Errorf("malformed http request: no header/body separator")
+		return nil, nil, fmt.Errorf("malformed http request: no header/body separator")
 	}
+	return b[:idx], b[idx+len(separator):], nil
+}
 
-	headerBlock := b[:idx]
-	req.Body = b[idx+len(separator):]
-
-	headerLines := bytes.Split(headerBlock, []byte("\r\n"))
+func splitHeader(header []byte) ([][]byte, error) {
+	headerLines := bytes.Split(header, []byte("\r\n"))
 	if len(headerLines) < 1 {
-		return req, fmt.Errorf("malformed http request: no request line")
+		return nil, fmt.Errorf("malformed http request: no request line")
 	}
+	return headerLines, nil
+}
 
-	// requestLine contains: GET /url HTTP1.1
-	requestLine := bytes.Split(headerLines[0], []byte(" "))
-	if len(requestLine) != 3 {
-		return req, fmt.Errorf("malformed http request: request line incomplete")
+func parseRequestLine(line []byte, req *Request) error {
+	parts := bytes.Split(line, []byte(" "))
+	if len(parts) != 3 {
+		return fmt.Errorf("malformed http request: request line incomplete")
 	}
+	req.Method = string(parts[0])
+	req.Protocol = string(parts[2])
 
-	req.Method = string(requestLine[0])
-	req.Protocol = string(requestLine[2])
+	return parseURL(string(parts[1]), req)
+}
 
-	url := string(requestLine[1])
-	url = path.Clean(url)
+func parseURL(rawUrl string, req *Request) error {
+	url := path.Clean(rawUrl)
 	req.Url = url
 
 	// url might contain: /url?key=value&foo=bar
 	urlSplitted := strings.Split(url, "?")
 	if len(urlSplitted) > 2 {
-		return req, fmt.Errorf("malformed http request: url contains more than one '?': %s", url)
+		return fmt.Errorf("malformed http request: url contains more than one '?': %s", req.Url)
 	}
 
 	if len(urlSplitted) == 2 {
 		req.Url = urlSplitted[0]
 		req.QueryParams = map[string]string{}
 		params := strings.Split(urlSplitted[1], "&")
+
 		for _, param := range params {
 			kv := strings.Split(param, "=")
 			if len(kv) == 2 {
@@ -69,13 +95,28 @@ func ParseRequest(b []byte) (Request, error) {
 		}
 	}
 
-	for _, h := range headerLines[1:] {
-		if req.Headers == nil {
-			req.Headers = map[string]string{}
-		}
-		header := strings.SplitN(string(h), ":", 2)
-		req.Headers[strings.TrimSpace(header[0])] = strings.TrimSpace(header[1])
+	return nil
+}
+
+func parseHeaders(headerLines [][]byte, req *Request) error {
+	if len(headerLines) == 0 {
+		return nil
 	}
 
-	return req, nil
+	req.Headers = make(map[string]string)
+	for _, h := range headerLines[1:] {
+		header := strings.SplitN(string(h), ":", 2)
+		if len(header) != 2 {
+			return fmt.Errorf("malformed http request: header incomplete")
+		}
+
+		key := strings.TrimSpace(header[0])
+		if key == "" {
+			return fmt.Errorf("malformed http request: empty header key")
+		}
+
+		req.Headers[key] = strings.TrimSpace(header[1])
+	}
+
+	return nil
 }
